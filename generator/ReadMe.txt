2788dba74734b5d1731cf3e356c8d3f7ee179668
atlan-antillia <atlan@antillia.com>
2024-01-28 20:26:57 +0900

    Initial commit

---
+++ generator/ReadMe.txt
@@ -0,0 +1,41 @@
+This dataset is provided with the following specifications:
+
+The dataset contains 50 brain T1-weighted MRI volumes with hippocampus labels. 
+Images are acquired at two different MRI units (with different field strengths) 
+and thus have very different resolutions and contrasts. Twenty of these images 
+have a higher resolution and are acquired with a relatively new 3T MRI system.
+
+
+Forty of these images belong to patients with temporal lobe epilepsy who may 
+have atrophic hippocampi. This makes the segmentation challenging and thus 
+evaluates the segmentation algorithms against practical problems. The remaining 
+10 images belong to nonepileptic subjects.
+
+
+We have divided the dataset into two groups of training and testing (50% each). 
+Training set is provided along with the labels. However, no label is provided for 
+the testing set. All images are in ANALYZE format.
+
+
+Special care has been taken to minimize the effect of partial volume in manual 
+drawings. The hippocampi were reconstructed in 3D to ensure that the outlines 
+in consecutive slices are consistent.
+
+
+You are encouraged to first work with the training set. If you are about to make 
+a journal publication and would like to report the segmentation results on the 
+testing set, you may send a request to Kourosh Jafari-Khouzani. You may then submit 
+your segmentation results. We will send you a table of evaluation metrics. Please use 
+"Hippocampus Segmentation Dataset" in the subject of your e-mails.
+
+
+You may read more about the dataset in the following paper:
+
+K. Jafari-Khouzani, K. Elisevich, S. Patel, and H. Soltanian-Zadeh, “Database of magnetic resonance
+images of nonepileptic subjects and temporal lobe epilepsy patients for validation of hippocampal
+segmentation techniques,” Neuroinformatics, 2011.
+
+Please refer to the above paper if you use this dataset in your publications. 
+
+Please send your comments to Kourosh Jafari-Khouzani.
+
---
+++ generator/create_base_dataset_512x512.py
@@ -0,0 +1,188 @@
+# Copyright 2024 antillia.com Toshiyuki Arai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# 2024/01/28 
+# create_base_dataset.py
+
+import os
+import sys
+import shutil
+import cv2
+
+import glob
+import numpy as np
+import math
+import nibabel as nib
+import traceback
+
+# Read file
+"""
+scan = nib.load('/path/to/stackOfimages.nii.gz')
+# Get raw data
+scan = scan.get_fdata()
+print(scan.shape)
+(num, width, height)
+
+"""
+
+
+# See : https://github.com/neheller/kits19/blob/master/starter_code/visualize.py
+
+
+
+H = 512
+W = 512
+
+# This function has been taken from visualize.py
+# https://github.com/neheller/kits19/blob/master/starter_code/visualize.py
+#
+def class_to_color(segmentation, kidney_color, tumor_color, tumor_only=True):
+    # initialize output to zeros
+    shp = segmentation.shape
+    seg_color = np.zeros((shp[0], shp[1], 3), dtype=np.float32)
+
+    # set output to appropriate color at each location
+    # 2023/08/10 antillia.com
+    if tumor_only:
+      # Set a kidney mask color to be black 
+      kidney_color = [0, 0, 0]
+    seg_color[np.equal(segmentation, 1)] = kidney_color
+    seg_color[np.equal(segmentation, 2)] = tumor_color
+    return seg_color
+  
+"""
+def get_mask_boundingbox( mask):
+    gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+
+    ret, bin_img = cv2.threshold(gray, 20, 255, cv2.THRESH_BINARY)
+
+    contours, hierarchy = cv2.findContours(
+       bin_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    #contours = list(filter(lambda x: cv2.contourArea(x) > 100, contours))
+    points = np.array(contours[0])
+    #print(points)
+    x, y, w, h = cv2.boundingRect(points)
+    rect = (x, y, w, h)
+    return rect
+"""
+
+
+def create_mask_files(img_file, name, output_dir, index):
+    print("--- niigz {}".format(img_file))
+    img = nib.load(img_file)
+    
+    data = img.get_fdata()
+    print("---data shape {} ".format(data.shape))
+    shape = data.shape
+    if len(shape) == 4:
+      data = data.reshape((shape[0], shape[1], shape[2]))
+
+    num_images = data.shape[2] # math.floor(data.shape[2]/2)
+    print("--- num_images {}".format(num_images))
+    num = 0
+  
+    for i in range(num_images):
+      img = data[:, :, i]
+      #img = class_to_color(img, KIDNEY_COLOR, TUMOR_COLOR) 
+      img = np.array(img)*255.0      
+      img = img.astype("uint8") 
+
+      filename = str(index + i) + "_" + name + ".jpg"
+      if np.any(img > 0):
+        filepath = os.path.join(output_dir, filename)
+        image = cv2.resize(img, (W, H))
+        cv2.imwrite(filepath, image)
+        print("Saved {}".format(filepath))
+        num += 1
+    return num
+  
+def create_image_files(image_file, name, output_masks_dir, output_images_dir, index):
+   
+    img = nib.load(image_file)
+
+    data = img.get_fdata()
+    print("---data shape {} ".format(data.shape))
+    shape = data.shape
+    if len(shape) == 4:
+      data = data.reshape((shape[0], shape[1], shape[2]))
+
+    num_images = data.shape[2] # math.floor(data.shape[2]/2)
+    print("--- num_images {}".format(num_images))
+    num = 0
+ 
+    for i in range(num_images):
+      img = data[:, :, i]
+      img = img.astype("uint8") 
+
+      filename = str(index + i) + "_" + name + ".jpg"
+
+      mask_filepath = os.path.join(output_masks_dir, filename)
+      if os.path.exists(mask_filepath):
+        filepath = os.path.join(output_images_dir, filename)
+        image = cv2.resize(img, (W, H))
+   
+        cv2.imwrite(filepath, image)
+        print("Saved {}".format(filepath))
+        num += 1
+    return num
+  
+
+def create_base_dataset(images_dir, labels_dir, output_images_dir, output_masks_dir):
+
+    image_files = glob.glob(images_dir + "/*.img")
+    index = 10000
+
+    for image_file in image_files:
+        basename = os.path.basename(image_file)
+        name     = basename.split(".")[0]
+        img_name     = basename.split(".")[0]
+        mask_name    = img_name + "_Hipp_Labels.img"
+        mask_file = os.path.join(labels_dir, mask_name)
+        print("---image file {}".format(image_file))
+
+        print("---mask file {}".format(mask_file))
+        
+        # 1 create mask files at first. 
+        num_masks  = create_mask_files(mask_file,   name, output_masks_dir,  index)
+        # 2 create image files if mask files exist.
+        num_images = create_image_files(image_file, name, output_masks_dir, output_images_dir, index)
+        print(" num_images: {}  num_masks: {}".format(num_images, num_masks))
+
+
+if __name__ == "__main__":
+  try:
+    images_dir        = "./Train"
+    labels_dir        = "./Train/Labels"
+    output_images_dir = "./Hippocampus-base/train/images/"
+    output_masks_dir  = "./Hippocampus-base/train/masks/"
+
+    if os.path.exists(output_images_dir):
+      shutil.rmtree(output_images_dir)
+    if not os.path.exists(output_images_dir):
+      os.makedirs(output_images_dir)
+
+    if os.path.exists(output_masks_dir):
+      shutil.rmtree(output_masks_dir)
+    if not os.path.exists(output_masks_dir):
+      os.makedirs(output_masks_dir)
+
+    # Create jpg image and mask files from nii.gz files under data_dir.
+    create_base_dataset(images_dir, labels_dir, output_images_dir, output_masks_dir)
+
+  except:
+    traceback.print_exc()
+
+
